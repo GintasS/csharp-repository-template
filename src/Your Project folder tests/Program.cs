95d6051359b7dfd08a209b07b46551511243ade8
GintasS <svedgintas@gmail.com>
2021-08-29 16:16:31 +0300

    Added default folders.

---
+++ src/Your Project folder tests/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StyleCopAnalyzersDemo
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
